db9e97a79ae807755ffdda7891b3c536875c2713
Ravindra <Ravindra@gmail.com>
2018-07-12 10:34:31 +0530

    Intial Commit

---
+++ src/main/java/mahindra/App.java
@@ -0,0 +1,13 @@
+package mahindra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
